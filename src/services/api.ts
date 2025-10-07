173f8c29a1844063603bae2a52f4fe2ef1f1be0c
emiran39 <emiran39@ford.com>
2025-10-07 18:32:37 -0300

    URL changed

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,4 +1,4 @@
-const BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:5173'
+const BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://task-class-api-latest.onrender.com'
 const JWT_KEY = import.meta.env.VITE_JWT_STORAGE_KEY || 'fiap.jwt'
 
 
